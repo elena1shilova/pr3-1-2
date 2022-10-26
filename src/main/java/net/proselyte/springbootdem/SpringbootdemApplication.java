5687c161ef22bb0e487f5dc2cf5f8f460bbeec0e
elena1989 <elena.shilova89@gmail.com>
2022-10-26 22:46:02 +0300

    начало проекта

---
+++ src/main/java/net/proselyte/springbootdem/SpringbootdemApplication.java
@@ -0,0 +1,13 @@
+package net.proselyte.springbootdem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdemApplication.class, args);
+	}
+
+}
---
+++ src/main/java/net/proselyte/springbootdem/model/Person.java
@@ -0,0 +1,28 @@
+package net.proselyte.springbootdem.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "person")
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String surname;
+
+    @Column
+    private Long age;
+
+    @Column
+    private String email;
+
+}
